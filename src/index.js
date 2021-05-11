c891248dd7ffff5115e9f28c0311b18f13db3c20
capuuu <capucinemoll@hotmail.com>
2021-05-11 12:09:22 +0200

    maj3

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import fastify from 'fastify'
 //on crée une application fastify en utilisant l'import de notre librairie ,
 //  on config fastify pour afficher des logs
 const app = fastify({ logger: true })
+//on crée une route fastify sur l'url "/"
+app.get('/', async () => {
+    return { text: "Welcome sur cette api!" }
+})
+
+
 //cette f° démarre notre serveur d'api
 const start = async () => {
     console.log('serveur qui run..')
     await app.listen(3000)
-    console.log('le serveur est lançé!  vous pouvez visiter sur: http://localhost:3000')
+    // ecoute sur le serveur 3000 toutes les rqtes
+    console.log('le serveur est lançé! Vous pouvez visiter sur: http://localhost:3000')
 }
-//lancement fonction démarrage
+// Et lancement fonction démarrage
 start()
